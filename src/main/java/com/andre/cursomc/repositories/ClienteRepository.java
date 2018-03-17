648708118111b6659335bb2afdeff9db9b5e7c68
Andre Costa <andrecostaa@gmail.com>
2018-03-17 10:26:30 -0300

    Cliente tipo cliente, telefones e enderecos

--- src/main/java/com/andre/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/andre/cursomc/repositories/ClienteRepository.java
@@ -3,9 +3,10 @@ package com.andre.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.andre.cursomc.domain.Cidade;
+import com.andre.cursomc.domain.Categoria;
+import com.andre.cursomc.domain.Cliente;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
 }
