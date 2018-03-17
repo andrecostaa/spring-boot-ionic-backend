3fa8c2bf72398f69fbca2c84aed9a52cef0566bd
Andre Costa <andrecostaa@gmail.com>
2018-03-17 10:35:14 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/andre/cursomc/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.andre.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andre.cursomc.domain.Cliente;
+import com.andre.cursomc.repositories.ClienteRepository;
+import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+	
+}
