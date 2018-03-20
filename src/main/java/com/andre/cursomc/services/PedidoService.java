30cee011409a56a3638a07dd64446febdc49c1e0
Andre Costa <andrecostaa@gmail.com>
2018-03-20 11:24:43 -0300

    EndPoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/andre/cursomc/services/PedidoService.java
@@ -0,0 +1,29 @@
+package com.andre.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andre.cursomc.domain.Cliente;
+import com.andre.cursomc.domain.Pedido;
+import com.andre.cursomc.repositories.PedidoRepository;
+import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+	
+}
