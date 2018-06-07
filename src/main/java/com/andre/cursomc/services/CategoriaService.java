2ea60334364f5e8bc2565611963e3b27a44626b9
Andre Costa <andrecostaa@gmail.com>
2018-06-07 14:35:05 -0300

    validacao sintatica com bean validation

--- src/main/java/com/andre/cursomc/services/CategoriaService.java
+++ src/main/java/com/andre/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.andre.cursomc.domain.Categoria;
+import com.andre.cursomc.dto.CategoriaDTO;
 import com.andre.cursomc.repositories.CategoriaRepository;
 import com.andre.cursomc.services.exceptions.DataIntegrityException;
 import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
