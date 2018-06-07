c951232d9818d8a7584751694444420b9d342127
Andre Costa <andrecostaa@gmail.com>
2018-06-07 17:53:27 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/andre/cursomc/services/CategoriaService.java
+++ src/main/java/com/andre/cursomc/services/CategoriaService.java
@@ -15,21 +15,20 @@ import com.andre.cursomc.repositories.CategoriaRepository;
 import com.andre.cursomc.services.exceptions.DataIntegrityException;
 import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
 
-
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
-		
+
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Categoria.class.getName());
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
 		}
-		
+
 		return obj;
 	}
 
@@ -39,8 +38,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
@@ -55,7 +55,7 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
@@ -64,5 +64,9 @@ public class CategoriaService {
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
-	
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+	}
+
 }
