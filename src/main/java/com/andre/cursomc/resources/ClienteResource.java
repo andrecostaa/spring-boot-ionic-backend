8780440153ca8dc08ab1a1ccea8e3d0497e4245d
Rotsen Albuquerque <rotsendiego@gmail.com>
2018-08-06 16:22:36 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/andre/cursomc/resources/ClienteResource.java
+++ src/main/java/com/andre/cursomc/resources/ClienteResource.java
@@ -1,6 +1,7 @@
 package com.andre.cursomc.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andre.cursomc.domain.Cliente;
 import com.andre.cursomc.dto.ClienteDTO;
+import com.andre.cursomc.dto.ClienteNewDTO;
 import com.andre.cursomc.services.ClienteService;
 
 
@@ -32,6 +35,17 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/andre/cursomc/services/ClienteService.java
+++ src/main/java/com/andre/cursomc/services/ClienteService.java
@@ -8,10 +8,17 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.andre.cursomc.domain.Cidade;
 import com.andre.cursomc.domain.Cliente;
+import com.andre.cursomc.domain.Endereco;
+import com.andre.cursomc.domain.enums.TipoCliente;
 import com.andre.cursomc.dto.ClienteDTO;
+import com.andre.cursomc.dto.ClienteNewDTO;
+import com.andre.cursomc.repositories.CidadeRepository;
 import com.andre.cursomc.repositories.ClienteRepository;
+import com.andre.cursomc.repositories.EnderecoRepository;
 import com.andre.cursomc.services.exceptions.DataIntegrityException;
 import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +28,11 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -33,6 +45,14 @@ public class ClienteService {
 		return obj;
 	}
 	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		
+		return obj;
+	}
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -60,6 +80,21 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
