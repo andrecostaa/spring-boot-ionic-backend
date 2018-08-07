ead48b066bb38efee6ae8cea49ed6434e41e008d
Andre Costa <andrecostaa@gmail.com>
2018-08-07 10:05:23 -0300

    subtotal e total do pedido

--- src/main/java/com/andre/cursomc/domain/Pedido.java
+++ src/main/java/com/andre/cursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -54,6 +53,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal() {
+		double soma = 0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
