7b92c5f2c12d443f71a543de62b2a800e58e8d23
Andre Costa <andrecostaa@gmail.com>
2018-08-08 00:44:39 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/andre/cursomc/services/PedidoService.java
+++ src/main/java/com/andre/cursomc/services/PedidoService.java
@@ -38,6 +38,9 @@ public class PedidoService {
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		
@@ -71,7 +74,9 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.save(obj.getItens());
-		System.out.println(obj);
+
+		emailService.sendOrderConfirmationEmail(obj);
+		
 		return obj;
 	}
 	
