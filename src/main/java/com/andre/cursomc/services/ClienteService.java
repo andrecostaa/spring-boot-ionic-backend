b05ac6792f2c027a2d50549b66e79f0a88a4e3ac
Andre Costa <andrecostaa@gmail.com>
2018-08-08 18:04:52 -0300

    Restricao de conteudo: cliente so recupera ele mesmo

--- src/main/java/com/andre/cursomc/services/ClienteService.java
+++ src/main/java/com/andre/cursomc/services/ClienteService.java
@@ -9,53 +9,60 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
 
 import com.andre.cursomc.domain.Cidade;
 import com.andre.cursomc.domain.Cliente;
 import com.andre.cursomc.domain.Endereco;
+import com.andre.cursomc.domain.enums.Perfil;
 import com.andre.cursomc.domain.enums.TipoCliente;
 import com.andre.cursomc.dto.ClienteDTO;
 import com.andre.cursomc.dto.ClienteNewDTO;
 import com.andre.cursomc.repositories.CidadeRepository;
 import com.andre.cursomc.repositories.ClienteRepository;
 import com.andre.cursomc.repositories.EnderecoRepository;
+import com.andre.cursomc.security.UserSS;
+import com.andre.cursomc.services.exceptions.AuthorizationException;
 import com.andre.cursomc.services.exceptions.DataIntegrityException;
 import com.andre.cursomc.services.exceptions.ObjectNotFoundException;
 
-
 @Service
 public class ClienteService {
+
 	@Autowired
 	private BCryptPasswordEncoder pe;
 
 	@Autowired
 	private ClienteRepository repo;
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+
 	public Cliente find(Integer id) {
-		Cliente obj = repo.findOne(id);
-		
-		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-					+ ", Tipo: " + Cliente.class.getName());
+
+		UserSS user = UserService.authenticated();
+		if (user == null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())) {
+			throw new AuthorizationException("Acesso negado");
 		}
-		
+
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
 		return obj;
 	}
-	
-	@Transactional
+
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
 		enderecoRepository.save(obj.getEnderecos());
-		
 		return obj;
 	}
-	
+
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
 		updateData(newObj, obj);
@@ -67,14 +74,14 @@ public class ClienteService {
 		try {
 			repo.delete(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados");
 		}
 	}
 
 	public List<Cliente> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
@@ -83,11 +90,13 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null, null);
 	}
-	
+
 	public Cliente fromDTO(ClienteNewDTO objDto) {
-		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(),
+				TipoCliente.toEnum(objDto.getTipo()), pe.encode(objDto.getSenha()));
 		Cidade cid = cidadeRepository.findOne(objDto.getCidadeId());
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
 		if (objDto.getTelefone2() != null) {
@@ -98,10 +107,9 @@ public class ClienteService {
 		}
 		return cli;
 	}
-	
+
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
-	
 }
---
+++ src/main/java/com/andre/cursomc/services/UserService.java
@@ -0,0 +1,17 @@
+package com.andre.cursomc.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.andre.cursomc.security.UserSS;
+
+public class UserService {
+	
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+}
