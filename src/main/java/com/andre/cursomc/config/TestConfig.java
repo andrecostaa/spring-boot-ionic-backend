94d21666ec5a20b6b2043fcb2a00551c2e22f5d9
Andre Costa <andrecostaa@gmail.com>
2018-08-08 14:48:31 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/andre/cursomc/config/TestConfig.java
+++ src/main/java/com/andre/cursomc/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.andre.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,6 +18,9 @@ public class TestConfig {
 	
 	@Autowired
 	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
 	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
@@ -28,5 +32,6 @@ public class TestConfig {
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
+	
 	
 }
