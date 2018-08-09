966c42ce29b8cbded126df67c161744305746329
Andre Costa <andrecostaa@gmail.com>
2018-08-09 11:30:12 -0300

    Esqueci a senha

--- src/main/java/com/andre/cursomc/services/EmailService.java
+++ src/main/java/com/andre/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.andre.cursomc.domain.Cliente;
 import com.andre.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,5 +16,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+
+	void sendNewPasswordEamil(Cliente cliente, String newPass);
 	
 }
